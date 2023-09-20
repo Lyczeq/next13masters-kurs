f41e3780d89777d20f287a1f92e7df1fc5803117
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-09-20 19:57:24 +0200

    feat: add fetching products from api

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,10 @@ export type Product = {
 	id: string;
 	category: string;
 	price: number;
-	name: string;
-	image: {
-		alt: string;
-		src: string;
-	};
+	title: string;
+	image: string;
+	// image: {
+	// 	alt: string;
+	// 	src: string;
+	// };
 };
