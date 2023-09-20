c198cf042aab64b8616eb7ea8f83eba9257ad71d
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-09-20 19:41:58 +0200

    fix ts

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-	return twMerge(clsx(inputs)) as string;
+export function cn(...inputs: ClassValue[]): string {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+	return twMerge(clsx(inputs));
 }
