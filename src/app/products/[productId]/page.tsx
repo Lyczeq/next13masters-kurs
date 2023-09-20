b6c096ded0b3e9260a301ea82cfef64954dfc682
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-09-20 21:01:18 +0200

    feat: generate single product metadata (#4)

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,23 @@
-import { ProductListItem } from "@/components/molecules/ProductListItem";
+import { type Metadata } from "next";
+import { ProductListItemCoverImage } from "@/components/atoms/ProductListItemCoverImage";
 import { type Product } from "@/types";
 
-type Props = {
+type Params = {
 	params: {
 		productId: string;
 	};
 };
+
+type Props = Params;
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+	const product = await getSingleProduct(params.productId);
+
+	return {
+		title: product.title,
+		description: product.description,
+	};
+}
 
 async function getSingleProduct(productId: string) {
 	const url = `https://naszsklep-api.vercel.app/api/products/${productId}`;
@@ -18,9 +30,17 @@ export default async function SingleProductPage({ params }: Props) {
 	const product = await getSingleProduct(params.productId);
 
 	return (
-		<div className="flex w-full flex-col items-center gap-4">
-			<ProductListItem product={product} />
-			<p>{product.description}</p>
+		<div>
+			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+				<div className="flex justify-center bg-slate-300 p-4">
+					<ProductListItemCoverImage src={product.image} alt={product.title} />
+				</div>
+				<div className="flex flex-col gap-4">
+					<h1>{product.title}</h1>
+					<p>{product.category}</p>
+					<p>{product.description}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/atoms/ActiveLink.tsx
+++ src/components/atoms/ActiveLink.tsx
@@ -24,8 +24,8 @@ export const ActiveLink = <T extends string>({ href, children, exact }: Props<T>
 
 	const isActive = exact ? pathname === hrefString : pathname.startsWith("/");
 
-	const styles = cn("rounded-2xl px-4 py-2 text-xl transition-colors hover:bg-slate-100", {
-		"bg-slate-100": isActive,
+	const styles = cn("px-4 py-2 text-xl transition-colors hover:border-b-2  hover:border-blue-300", {
+		"border-b-2 border-blue-300": isActive,
 	});
 
 	return (
