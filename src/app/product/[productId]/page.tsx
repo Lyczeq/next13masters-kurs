929556d2697f5786a5a0a4a242102c829eb82c6d
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-09-28 20:25:08 +0200

    feat: add codegen and use graphql endpoint for product list and single product page (#8)

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,6 +1,8 @@
 import { type Metadata } from "next";
+import { notFound } from "next/navigation";
 import { ProductListItemCoverImage } from "@/components/atoms/ProductListItemCoverImage";
-import { type Product } from "@/types";
+import { executeGraphql } from "@/api/graphqlApi";
+import { ProductGetByIdDocument } from "@/gql/graphql";
 
 type Params = {
 	params: {
@@ -10,41 +12,40 @@ type Params = {
 
 type Props = Params;
 
-export async function generateStaticParams() {
-	const url = `https://naszsklep-api.vercel.app/api/products`;
-	const response = await fetch(url);
-	const products = (await response.json()) as Product[];
-	return products.map((product) => product.id);
-}
+// export async function generateStaticParams() {
+// 	const url = `https://naszsklep-api.vercel.app/api/products`;
+// 	const response = await fetch(url);
+// 	const products = (await response.json()) as Product[];
+// 	return products.map((product) => product.id);
+// }
 
 export async function generateMetadata({ params }: Params): Promise<Metadata> {
-	const product = await getSingleProduct(params.productId);
+	const { product } = await executeGraphql(ProductGetByIdDocument, { id: params.productId });
 
 	return {
-		title: product.title,
-		description: product.description,
+		title: product?.name,
+		description: product?.description,
 	};
 }
 
-async function getSingleProduct(productId: string) {
-	const url = `https://naszsklep-api.vercel.app/api/products/${productId}`;
-
-	const response = await fetch(url);
-	return response.json() as Promise<Product>;
-}
-
 export default async function SingleProductPage({ params }: Props) {
-	const product = await getSingleProduct(params.productId);
+	const { product } = await executeGraphql(ProductGetByIdDocument, { id: params.productId });
+
+	if (!product) {
+		throw notFound();
+	}
 
 	return (
 		<div>
 			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
 				<div className="flex justify-center bg-slate-300 p-4">
-					<ProductListItemCoverImage src={product.image} alt={product.title} />
+					{product.images[0] && (
+						<ProductListItemCoverImage src={product.images[0].url} alt={product.name} />
+					)}
 				</div>
 				<div className="flex flex-col gap-4">
-					<h1>{product.title}</h1>
-					<p>{product.category}</p>
+					<h1>{product.name}</h1>
+					{product.categories[0] && <p>{product.categories[0].name}</p>}
 					<p>{product.description}</p>
 				</div>
 			</div>
--- src/components/molecules/ProductListItem.tsx
+++ src/components/molecules/ProductListItem.tsx
@@ -1,25 +1,24 @@
 import Link from "next/link";
 import { ProductListItemCoverImage } from "../atoms/ProductListItemCoverImage";
-import { type Product } from "@/types";
+import { type ProductListItemFragment } from "@/gql/graphql";
 
 type Props = {
-	product: Product;
+	product: ProductListItemFragment;
 };
 
 function formatPrice(price: number) {
 	return Intl.NumberFormat("en-us", { style: "currency", currency: "USD" }).format(price);
 }
 
-export const ProductListItem = ({ product: { image, category, title, price, id } }: Props) => {
+export const ProductListItem = ({ product: { images, categories, price, id, name } }: Props) => {
 	return (
 		<li className="rounded-2xl border bg-slate-300">
 			<Link href={`/product/${id}`}>
 				<div className="flex flex-col gap-2 p-2">
-					<h3>
-						{title} | {category}
-					</h3>
+					<h3>{name}</h3>
+					{categories.at(0) && <span>{categories[0].name}</span>}
 					<p>{formatPrice(price)}</p>
-					<ProductListItemCoverImage src={image} alt={title} />
+					{images[0] && <ProductListItemCoverImage src={images[0].url} alt={name} />}
 				</div>
 			</Link>
 		</li>
