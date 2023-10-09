b87cc416c56350e0666fef7e765180a0439b99bb
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-10-09 20:20:55 +0200

    feat: add header and navbar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/utils/cn";
 import { ActiveLink } from "@/components/atoms/ActiveLink";
 import { SearchInput } from "@/components/molecules/SearchInput";
 import Link from "next/link";
+import { getCartFromCookies } from "@/api/cart";
+import { Header } from "@/components/molecules/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,31 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="en">
 			<body className={(cn(inter.className), "flex flex-col items-center px-4")}>
-				<header className="flex max-w-screen-xl items-center gap-10 py-4">
-					<nav className="w-full">
-						<ul className="flex w-full justify-center gap-4">
-							<li>
-								<ActiveLink href="/" exact>
-									Home
-								</ActiveLink>
-							</li>
-							<li>
-								<ActiveLink href="/products">All</ActiveLink>
-							</li>
-							<li>
-								<ActiveLink href="/categories/t-shirts/1">T-shirts</ActiveLink>
-							</li>
-							<li>
-								<ActiveLink href="/categories/hoodies/1">Hoodies</ActiveLink>
-							</li>{" "}
-							<li>
-								<ActiveLink href="/categories/accessories/1">Accessories</ActiveLink>
-							</li>
-						</ul>
-					</nav>
-					<SearchInput />
-					<Link href="/cart">Cart</Link>
-				</header>
+				<Header />
 				{children}
 			</body>
 		</html>
