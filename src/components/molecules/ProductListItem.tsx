3563c61846563b0320532a6ab75c5649b948a6f0
Jakub Lyczko <jakub.lyczko@swmansion.com>
2023-10-13 21:27:43 +0200

    feat add avg rating on list item

--- src/components/molecules/ProductListItem.tsx
+++ src/components/molecules/ProductListItem.tsx
@@ -7,14 +7,24 @@ type Props = {
 	product: ProductListItemFragment;
 };
 
-export const ProductListItem = ({ product: { images, categories, price, id, name } }: Props) => {
+export const ProductListItem = ({
+	product: { images, categories, price, id, name, reviews },
+}: Props) => {
+	const reviewsCount = reviews.length;
+
+	const averageRating =
+		reviews.reduce((accumulator, currentReview) => {
+			return (accumulator += currentReview.rating);
+		}, 0) / reviewsCount;
+
 	return (
 		<li className="rounded-2xl border bg-slate-300">
 			<Link href={`/product/${id}`}>
 				<div className="flex flex-col gap-2 p-2">
 					<h3>{name}</h3>
 					{categories.at(0) && <span>{categories[0].name}</span>}
-					<p>{formatPrice(price)}</p>
+					<p data-testid="product-price">{formatPrice(price)}</p>
+					<p data-testid="product-rating">{averageRating}</p>
 					{images[0] && <ProductListItemCoverImage src={images[0].url} alt={name} />}
 				</div>
 			</Link>
